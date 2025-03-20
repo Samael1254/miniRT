ef946e85491b5fc34ffd999631e85b33bfa637b6
gfulconi <gfulconi@student.42.fr>
2025-03-20 12:25:05 +0100

    fixed normal maps

--- srcs_bonus/raytracing/normals_bonus.c
+++ srcs_bonus/raytracing/normals_bonus.c
@@ -2,6 +2,7 @@
 #include "ft_math.h"
 #include "minirt_bonus.h"
 #include "minirt_defs_bonus.h"
+#include <math.h>
 
 static t_vector3d	sphere_normal(t_sphere sphere, t_vector3d point)
 {
@@ -49,22 +50,40 @@ t_vector3d	color_to_normal(t_color color)
 {
 	t_vector3d	normal;
 
-	normal.x = -(double)color.r / 127.5 + 1;
-	normal.y = -(double)color.g / 127.5 + 1;
-	normal.z = -(double)color.b / 127.5 + 1;
+	normal.x = (double)color.r / 127.5 - 1;
+	normal.y = (double)color.g / 127.5 - 1;
+	normal.z = (double)color.b / 127.5 - 1;
 	return (normal);
+}
+
+t_vector3d	tangent_to_world(t_vector3d normal_tx, t_vector3d normal_world)
+{
+	t_vector3d	r;
+	t_vector3d	tangent;
+	t_vector3d	bitangent;
+	t_vector3d	new_normal;
+
+	if (ft_inff(fabs(normal_world.y), 0.9))
+		r = ft_set_vector3d(0, 1, 0);
+	else
+		r = ft_set_vector3d(1, 0, 0);
+	tangent = ft_normalize_vector3d(ft_cross_vectors3d(r, normal_tx));
+	bitangent = ft_normalize_vector3d(ft_cross_vectors3d(normal_tx, tangent));
+	new_normal = ft_normalize_vector3d(ft_add_vectors3d(ft_add_vectors3d(ft_scale_vector3d(normal_tx.x,
+						tangent), ft_scale_vector3d(normal_tx.y, bitangent)),
+				ft_scale_vector3d(normal_tx.z, normal_world)));
+	return (new_normal);
 }
 
 t_vector3d	blend_normal_map(t_vector2d uv, t_vector3d normal,
 		t_material material)
 {
-	t_vector3d	new_normal;
+	t_vector3d	normal_tx;
 
 	if (!material.img_normal.img)
 		return (normal);
-	new_normal = color_to_normal(get_pixel_color(material.img_normal, uv));
-	(void)new_normal;
-	return (ft_lerp3d(normal, new_normal, 0.5));
+	normal_tx = color_to_normal(get_pixel_color(material.img_normal, uv));
+	return (tangent_to_world(normal_tx, normal));
 }
 
 t_vector3d	normal_at_point(t_object object, t_intersection inter,
