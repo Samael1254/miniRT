e69a49062bfe14e0c356e184e225d1b5b3fa8e61
gfulconi <gfulconi@student.42.fr>
2025-04-03 11:51:15 +0200

    polished bounding box

--- srcs_bonus/bvh/intersect_aabb_bonus.c
+++ srcs_bonus/bvh/intersect_aabb_bonus.c
@@ -4,9 +4,8 @@
 #include "minirt_intersections_bonus.h"
 #include <math.h>
 #include <stdbool.h>
-#include <stdio.h>
 
-t_vec3	ft_inverse_vec3(t_vec3 v)
+static t_vec3	ft_inverse_vec3(t_vec3 v)
 {
 	t_vec3	u;
 
@@ -20,14 +19,14 @@ t_vec3	ft_inverse_vec3(t_vec3 v)
 	return (u);
 }
 
-void	ft_vec3_to_array(t_vec3 v, double a[3])
+static void	ft_vec3_to_array(t_vec3 v, double a[3])
 {
 	a[0] = v.x;
 	a[1] = v.y;
 	a[2] = v.z;
 }
 
-void	ft_swapf(double *a, double *b)
+static void	ft_swapf(double *a, double *b)
 {
 	double	temp;
 
@@ -62,20 +61,20 @@ bool	aabb_roots(double tbox[2], const double dets[3], const double omin[3],
 	i = 0;
 	while (i < 3)
 	{
-		// if (isinf(dets[i]))
-		// {
-		// 	if (ft_supf(omin[i], 0) || ft_inff(omax[i], 0))
-		// 		return (false);
-		// 	i++;
-		// 	continue ;
-		// }
+		if (isinf(dets[i]))
+		{
+			if (ft_supf(omin[i], 0) || ft_inff(omax[i], 0))
+				return (false);
+			i++;
+			continue ;
+		}
 		tplane[0] = omin[i] * dets[i];
 		tplane[1] = omax[i] * dets[i];
 		if (ft_supf(tplane[0], tplane[1]))
 			ft_swapf(&tplane[0], &tplane[1]);
 		tbox[0] = fmax(tbox[0], tplane[0]);
 		tbox[1] = fmin(tbox[1], tplane[1]);
-		if (tbox[0] > tbox[1])
+		if (ft_supf(tbox[0], tbox[1]))
 			return (false);
 		i++;
 	}
@@ -97,53 +96,10 @@ double	intersect_aabb(t_ray ray, t_aabb box)
 	tbox[1] = INFINITY;
 	if (!aabb_roots(tbox, dets, omin, omax))
 	{
-		hit_box(false, false);
+		hit_box(false, false); // test to delete
 		return (INFINITY);
 	}
-	hit_box(true, false);
+	hit_box(true, false); // test to delete
 	t = closest_root(tbox[0], tbox[1]);
 	return (t);
 }
-
-// double	intersect_aabb(t_ray r, t_aabb box)
-// {
-// 	double	tmin;
-// 	double	tmax;
-// 	double	tymin;
-// 	double	tymax;
-// 	double	tzmin;
-// 	double	tzmax;
-//
-// 	tmin = (box.min.x - r.origin.x) / r.direction.x;
-// 	tmax = (box.max.x - r.origin.x) / r.direction.x;
-// 	if (tmin > tmax)
-// 		ft_swapf(&tmin, &tmax);
-// 	tymin = (box.min.y - r.origin.y) / r.direction.y;
-// 	tymax = (box.max.y - r.origin.y) / r.direction.y;
-// 	if (tymin > tymax)
-// 		ft_swapf(&tymin, &tymax);
-// 	if ((tmin > tymax) || (tymin > tmax))
-// 	{
-// 		hit_box(false, false);
-// 		return (INFINITY);
-// 	}
-// 	if (tymin > tmin)
-// 		tmin = tymin;
-// 	if (tymax < tmax)
-// 		tmax = tymax;
-// 	tzmin = (box.min.z - r.origin.z) / r.direction.z;
-// 	tzmax = (box.max.z - r.origin.z) / r.direction.z;
-// 	if (tzmin > tzmax)
-// 		ft_swapf(&tzmin, &tzmax);
-// 	if ((tmin > tzmax) || (tzmin > tmax))
-// 	{
-// 		hit_box(false, false);
-// 		return (INFINITY);
-// 	}
-// 	if (tzmin > tmin)
-// 		tmin = tzmin;
-// 	if (tzmax < tmax)
-// 		tmax = tzmax;
-// 	hit_box(true, false);
-// 	return (tmin);
-// }
