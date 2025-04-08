0a2434f0dff7faaa753957cfc635c4f104e20674
gfulconi <gfulconi@student.42.fr>
2025-04-08 13:15:55 +0200

    debugging triangle aabb overlap

--- srcs_bonus/bvh/aabb_overlaps_bonus.c
+++ srcs_bonus/bvh/aabb_overlaps_bonus.c
@@ -1,5 +1,4 @@
 #include "ft_algebra.h"
-#include "minirt_aabb_overlap_bonus.h"
 #include "minirt_defs_bonus.h"
 #include <math.h>
 #include <stdbool.h>
@@ -14,7 +13,7 @@ static t_vec3	ft_abs_vec3(t_vec3 v)
 	return (u);
 }
 
-static bool	aabb_aabb_overlap(t_aabb a, t_aabb b)
+bool	aabb_aabb_overlap(t_aabb a, t_aabb b)
 {
 	if (a.min.x > b.max.x || a.max.x < b.min.x)
 		return (false);
@@ -30,7 +29,7 @@ static t_vec3	aabb_center(t_aabb box)
 	return (ft_scale_vec3(0.5, ft_add_vec3(box.min, box.max)));
 }
 
-static bool	plane_aabb_overlap(t_vec3 p_normal, t_vec3 p_point, t_aabb box)
+bool	plane_aabb_overlap(t_vec3 p_normal, t_vec3 p_point, t_aabb box)
 {
 	t_vec3	c;
 	t_vec3	h;
@@ -44,34 +43,69 @@ static bool	plane_aabb_overlap(t_vec3 p_normal, t_vec3 p_point, t_aabb box)
 	return (fabs(s) <= r);
 }
 
-static bool	test3(t_vec3 r_vertices[3], t_vec3 edges[3], t_vec3 extent,
+bool	test3(t_vec3 r_vertices[3], t_vec3 edges[3], t_vec3 extent,
 		t_vec3 unit[3])
 {
-	if (!axis1(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis2(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis3(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis4(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis5(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis6(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis7(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis8(unit, edges, r_vertices, extent))
-		return (false);
-	if (!axis9(unit, edges, r_vertices, extent))
-		return (false);
+	double	radius;
+	t_vec3	axes[9];
+	double	p[3];
+	int		i;
+	int		j;
+
+	axes[0] = ft_cross_vec3(unit[0], edges[0]);
+	axes[1] = ft_cross_vec3(unit[0], edges[1]);
+	axes[2] = ft_cross_vec3(unit[0], edges[2]);
+	axes[3] = ft_cross_vec3(unit[1], edges[0]);
+	axes[4] = ft_cross_vec3(unit[1], edges[1]);
+	axes[5] = ft_cross_vec3(unit[1], edges[2]);
+	axes[6] = ft_cross_vec3(unit[2], edges[0]);
+	axes[7] = ft_cross_vec3(unit[2], edges[1]);
+	axes[8] = ft_cross_vec3(unit[2], edges[2]);
+	i = -1;
+	while (++i < 9)
+	{
+		if (ft_vec3_norm(axes[i]) < 1e-8)
+			continue ;
+		radius = ft_dot_vec3(extent, ft_abs_vec3(axes[i]));
+		j = -1;
+		while (++j < 3)
+			p[j] = ft_dot_vec3(axes[i], r_vertices[j]);
+		if (fmin(p[0], fmin(p[1], p[2])) > radius || fmax(p[0], fmax(p[1],
+					p[2])) < -radius)
+			return (false);
+	}
 	return (true);
 }
 
-static t_aabb	create_triangle_aabb(const t_vec3 vertices[3])
+// bool	test3(t_vec3 r_vertices[3], t_vec3 edges[3], t_vec3 extent,
+// 		t_vec3 unit[3])
+// {
+// 	if (!axis1(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis2(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis3(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis4(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis5(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis6(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis7(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis8(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	if (!axis9(unit, edges, r_vertices, extent))
+// 		return (false);
+// 	return (true);
+// }
+
+t_aabb	create_triangle_aabb(const t_vec3 vertices[3])
 {
 	unsigned int	i;
 	t_aabb			aabb;
+	const double	epsilon = 1e-5;
 
 	i = 0;
 	aabb.min = ft_init_vec3(INFINITY);
@@ -86,13 +120,16 @@ static t_aabb	create_triangle_aabb(const t_vec3 vertices[3])
 		aabb.max.z = fmax(aabb.max.z, vertices[i].z);
 		i++;
 	}
+	aabb.min = ft_sub_vec3(aabb.min, ft_init_vec3(epsilon));
+	aabb.max = ft_add_vec3(aabb.max, ft_init_vec3(epsilon));
 	return (aabb);
 }
 
-bool	triangle_aabb_overlap(unsigned int vertices_id[3], t_aabb box,
+bool	triangle_aabb_overlap(const unsigned int vertices_id[3], t_aabb box,
 		const t_vec3 *vertices)
 {
 	t_vec3	unit[3];
+	t_vec3	t_vertices[3];
 	t_vec3	r_vertices[3];
 	t_vec3	edges[3];
 	t_vec3	center;
@@ -100,21 +137,25 @@ bool	triangle_aabb_overlap(unsigned int vertices_id[3], t_aabb box,
 
 	center = aabb_center(box);
 	extent = ft_sub_vec3(box.max, center);
-	r_vertices[0] = ft_sub_vec3(vertices[vertices_id[0]], center);
-	r_vertices[1] = ft_sub_vec3(vertices[vertices_id[1]], center);
-	r_vertices[2] = ft_sub_vec3(vertices[vertices_id[2]], center);
+	t_vertices[0] = vertices[vertices_id[0]];
+	t_vertices[1] = vertices[vertices_id[1]];
+	t_vertices[2] = vertices[vertices_id[2]];
+	r_vertices[0] = ft_sub_vec3(t_vertices[0], center);
+	r_vertices[1] = ft_sub_vec3(t_vertices[1], center);
+	r_vertices[2] = ft_sub_vec3(t_vertices[2], center);
 	edges[0] = ft_sub_vec3(r_vertices[1], r_vertices[0]);
 	edges[1] = ft_sub_vec3(r_vertices[2], r_vertices[1]);
 	edges[2] = ft_sub_vec3(r_vertices[0], r_vertices[2]);
 	unit[0] = ft_set_vec3(1, 0, 0);
 	unit[1] = ft_set_vec3(0, 1, 0);
 	unit[2] = ft_set_vec3(0, 0, 1);
-	if (!test3(r_vertices, edges, extent, unit))
+	// if (!test3(r_vertices, edges, extent, unit))
+	// 	return (false);
+	if (!aabb_aabb_overlap(box, create_triangle_aabb(t_vertices)))
 		return (false);
-	if (!aabb_aabb_overlap(box, create_triangle_aabb(r_vertices)))
-		return (false);
-	if (!plane_aabb_overlap(ft_cross_vec3(edges[0], edges[1]), r_vertices[0],
-			box))
-		return (false);
+	// if (!plane_aabb_overlap(ft_normalize_vec3(ft_cross_vec3(edges[0],
+	// 				edges[1])), t_vertices[0], box))
+	// return (false);
+	(void)extent;
 	return (true);
 }
