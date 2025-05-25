45a7126f17a7d56dcfa1213994f8a4a200043401
gfulconi <fulconis.guillaume.foe@gmail.com>
2025-05-25 15:15:07 +0200

    cleaned errors.c

--- srcs/errors/errors.c
+++ srcs/errors/errors.c
@@ -1,67 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   errors.c                                     :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: macuesta <macuesta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 17:21:29 by macuesta          #+#    #+#             */
-/*   Updated: 2025/04/14 17:21:29 by macuesta         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:14:43 by gfulconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_strings.h"
-#include "minirt_base.h"
 #include <stdbool.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 
-unsigned int	warnings_number(bool add)
+void	error(const char *type, const char *msg)
 {
-	static int	n_errors;
-
-	if (add)
-		n_errors++;
-	return (n_errors);
+	dprintf(2, "\e[31;1mError\e[0;1m: ");
+	if (type)
+		dprintf(2, "%s\e[0m: ", type);
+	dprintf(2, "%s\n", msg);
 }
 
-void	warning(char const *type, char const *msg)
+void	warning(const char *type, const char *msg)
 {
-	write(2, "\e[33;1mWarning: \e[0m", 20);
+	dprintf(2, "\e[33;1mWarning\e[0;1m: ");
 	if (type)
-	{
-		write(2, "\e[1m", 4);
-		write(2, type, ft_strlen(type));
-		write(2, ": \e[0m", 6);
-	}
-	write(2, msg, ft_strlen(msg));
-	write(2, "\n", 1);
-	warnings_number(true);
+		dprintf(2, "%s\e[0m: ", type);
+	dprintf(2, "%s\n", msg);
 }
 
-void	info(char const *type, char const *msg)
+void	info(const char *type, const char *msg)
 {
-	write(2, "\e[34;1mInfo: \e[0m", 17);
+	dprintf(2, "\e[34;1mInfo\e[0;1m: ");
 	if (type)
-	{
-		write(2, "\e[1m", 4);
-		write(2, type, ft_strlen(type));
-		write(2, ": \e[0m", 6);
-	}
-	write(2, msg, ft_strlen(msg));
-	write(2, "\n", 1);
-}
-
-void	error(char const *type, char const *msg, t_state *state)
-{
-	write(2, "\e[31;1mError: \e[0m", 18);
-	if (type)
-	{
-		write(2, "\e[1m", 4);
-		write(2, type, ft_strlen(type));
-		write(2, ": \e[0m", 6);
-	}
-	write(2, msg, ft_strlen(msg));
-	write(2, "\n", 1);
-	exit_program(state, EXIT_FAILURE);
+		dprintf(2, "%s\e[0m: ", type);
+	dprintf(2, "%s\n", msg);
 }
