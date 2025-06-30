0e9b299a3add380805423b414267ca326ae8a3fd
gfulconi <gfulconi@student.42.fr>
2025-06-30 17:12:25 +0200

    fixed bold text in info messages

--- srcs/errors/errors.c
+++ srcs/errors/errors.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   errors.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: macuesta <macuesta@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/14 17:21:29 by macuesta          #+#    #+#             */
-/*   Updated: 2025/06/02 00:48:03 by gfulconi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minirt_base.h"
 #include "minirt_defs.h"
 #include <stdbool.h>
@@ -22,7 +10,7 @@ void	error(const char *type, const char *msg, t_state *state)
 	dprintf(2, "\e[31;1mError\e[0;1m: ");
 	if (type)
 		dprintf(2, "%s\e[0m: ", type);
-	dprintf(2, "%s\n", msg);
+	dprintf(2, "%s\e[0m\n", msg);
 	exit_program(state, EXIT_FAILURE);
 }
 
@@ -31,7 +19,7 @@ void	warning(const char *type, const char *msg)
 	dprintf(2, "\e[33;1mWarning\e[0;1m: ");
 	if (type)
 		dprintf(2, "%s\e[0m: ", type);
-	dprintf(2, "%s\n", msg);
+	dprintf(2, "%s\e[0m\n", msg);
 }
 
 void	info(const char *type, const char *msg)
@@ -39,5 +27,5 @@ void	info(const char *type, const char *msg)
 	dprintf(2, "\e[34;1mInfo\e[0;1m: ");
 	if (type)
 		dprintf(2, "%s\e[0m: ", type);
-	dprintf(2, "%s\n", msg);
+	dprintf(2, "%s\e[0m\n", msg);
 }
